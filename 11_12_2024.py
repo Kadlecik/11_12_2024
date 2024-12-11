7c8898da0d8aa3f71f196cd9e4dfafa63706126f
Kadlecik <ondrej.kadlecik@seznam.cz>
2024-12-11 18:42:24 +0100

    doplnění

--- 11_12_2024.py
+++ 11_12_2024.py
@@ -1,1 +1,13 @@
 
+import json
+
+from moje.lekce_1 import print
+
+data = {"name": "John",
+        "age": 25
+        "kids": [],
+        "married": False}
+
+j_data = json.dums(False)
+print(j_data)
+print(data)
