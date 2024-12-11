a7a6dcb84b1bd5844f8613175e79288f5500010c
Kadlecik <ondrej.kadlecik@seznam.cz>
2024-12-11 19:15:39 +0100

    doplnění

--- 11_12_2024.py
+++ 11_12_2024.py
@@ -4,10 +4,10 @@ import json
 
 
 data = {"name": "John",
-        "age": 25
+        "age": 25,
         "kids": [],
         "married": False}
 
-j_data = json.dums(data)
+j_data = json.dumps(data)
 print(j_data)
 print(data)
